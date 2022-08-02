029d42c4bc4eaa65c0154b50caf4acaeeff98e99
hongjingmao <hj.mao@outlook.com>
2022-08-02 09:53:20 +0800

    1

--- App/App.h
+++ App/App.h
@@ -37,7 +37,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-
+#include <iostream>
 #include "sgx_error.h"       /* sgx_status_t */
 #include "sgx_eid.h"     /* sgx_enclave_id_t */
 
